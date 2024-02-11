85c9a11faa1c6b56bb858344efdfeb1a0abc44c0
OnSake <julesmuller06@gmail.com>
2024-02-11 13:29:45 +0100

    community page add

--- Screens/save_in_bdd.py
+++ Screens/save_in_bdd.py
@@ -5,6 +5,6 @@ connexion = sqlite3.connect("BDD/turtle_BDD.db")
 curseur = connexion.cursor()
 
 def save(username, filename):
-  curseur.execute(f'INSERT INTO Turtle_oeuvre (username, nom_oeuvre) VALUES ("{username}", "{filename}")')
+  curseur.execute(f'INSERT INTO Turtle_oeuvre (username, nom_oeuvre, user_id) VALUES ("{username}", "{filename}"')
   connexion.commit()
   connexion.close() #pour arreter
